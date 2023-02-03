b090d78accb0ba70296215437861a2bf1a269fee
Aleksander Alimov <73174419+akella64@users.noreply.github.com>
2023-02-03 12:32:08 +0400

    first commit

---
+++ app/components/layout/header/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+import styles from './Header.module.scss'
+
+const Header: FC = () => {
+	return <header className={styles.header}>
+				Header
+	</header>
+}
+
+export default Header
---
+++ app/components/screens/home/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import Layout from '@/layout/Layout'
+
+const Home: FC = () => {
+	return (
+		<Layout title='Home'>
+			Nextjs template
+		</Layout>
+	)
+}
+
+export default Home
